28030d91e83e1f5055e311f54decdb1c41517e2c
YeonghunKO <yhko1988@gmail.com>
2022-11-12 19:44:44 +0900

    fix : vite.config.ts 에도 절대경로 표시해줘야함
    그리고 node module을 다운받아야함

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
-import test from '@components/test';
+import Test from '@components/test';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Test />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
