dc8fafbf0c5c4974bdef98bcdebd8a7264b09256
moonkorea00 <moonkorea00@gmail.com>
2022-11-13 02:52:19 +0900

    feat : theme provider

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+}
+input, button {
+  outline: none;
+  border: none;
+  cursor: pointer;
+}
+`;
+
+export default GlobalStyles;
