e51825dcca1c1bcd806607cb39f44835c2be5bb1
YeonghunKO <yhko1988@gmail.com>
2022-11-13 14:27:10 +0900

    feat : 로그인 input value 통제가능하게 만듬

--- src/pages/LogIn/LogIn.style.ts
+++ src/pages/LogIn/LogIn.style.ts
@@ -6,3 +6,32 @@ export const LogInWrapper = styled.section`
   height: 100vh;
   width: 100vw;
 `;
+
+export const LogInForm = styled.form`
+  ${({ theme }) => theme.flexColumn};
+  background: ${({ theme }) => theme.bg.white};
+  border-radius: 10px;
+  width: 40%;
+  height: 70%;
+  padding: 1rem;
+`;
+
+export const LogInInput = styled.input`
+  width: 90%;
+  padding: 1rem;
+  border-radius: 10px;
+  margin: 1rem;
+  border: 2px solid ${({ theme }) => theme.bg.grey};
+  :focus {
+    outline: none;
+  }
+`;
+
+export const LogInButton = styled.button`
+  margin: 1rem;
+  border-radius: 10px;
+  color: ${({ theme }) => theme.bg.grey};
+  padding: 1rem;
+  width: 100%;
+  background-color: ${({ theme }) => theme.bg.darkBlue};
+`;
--- src/pages/LogIn/Login.tsx
+++ src/pages/LogIn/Login.tsx
@@ -1,7 +1,66 @@
-import { LogInWrapper } from './LogIn.style';
+import {
+  LogInButton,
+  LogInForm,
+  LogInInput,
+  LogInWrapper,
+} from './LogIn.style';
+import { TextField, InputAdornment } from '@mui/material';
+import Icons from '@components/common/Icons';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useSignForm from '@hooks/useSignForm';
+import { investmentService } from '@apis/index';
 
 const LogIn = () => {
-  return <LogInWrapper>LogIn</LogInWrapper>;
+  const navigate = useNavigate();
+  const { userInfo, handleInputValue } = useSignForm();
+
+  const handleLogInButton = async () => {
+    const token = await investmentService.logIn(userInfo);
+    console.log(token);
+
+    // localStorage.setItem('token');
+    // navigate('/dashboard');
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      navigate('/dashboard');
+    }
+  }, []);
+  return (
+    <LogInWrapper>
+      <LogInForm onClick={e => e.preventDefault()}>
+        <TextField
+          variant="outlined"
+          id="input-with-icon-textfield"
+          InputProps={{
+            startAdornment: (
+              <Icons.User width="1.5rem" height="1.5rem" marginRight="1rem" />
+            ),
+          }}
+          placeholder="이메일을 입력해주세요"
+          fullWidth
+          margin="normal"
+          onChange={handleInputValue('email')}
+        />
+        <TextField
+          variant="outlined"
+          id="input-with-icon-textfield"
+          InputProps={{
+            startAdornment: (
+              <Icons.Lock width="1.5rem" height="1.5rem" marginRight="1rem" />
+            ),
+          }}
+          placeholder="비밀번호를 입력해주세요"
+          fullWidth
+          margin="normal"
+          onChange={handleInputValue('password')}
+        />
+        <LogInButton onClick={handleLogInButton}>로그인</LogInButton>
+      </LogInForm>
+    </LogInWrapper>
+  );
 };
 
 export default LogIn;
