7c65be8a931dbaad7915adb31a9acccf255085ac
YeonghunKO <yhko1988@gmail.com>
2022-11-13 13:15:01 +0900

    chore : 절대경로 추가

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,22 @@ export default defineConfig({
         find: '@components',
         replacement: resolve(__dirname, 'src/components'),
       },
+      {
+        find: '@apis',
+        replacement: resolve(__dirname, 'src/apis'),
+      },
+      {
+        find: '@pages',
+        replacement: resolve(__dirname, 'src/pages'),
+      },
+      {
+        find: '@shared',
+        replacement: resolve(__dirname, 'src/shared'),
+      },
+      {
+        find: '@utils',
+        replacement: resolve(__dirname, 'src/utils'),
+      },
     ],
   },
 
