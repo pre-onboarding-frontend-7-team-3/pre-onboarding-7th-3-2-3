4c6abfcbc3731222c780905d17c56a88c4f54ca2
leejiwon <365support@naver.com>
2022-11-15 05:53:50 +0900

    fix: suspense 주석

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,27 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { RecoilRoot } from 'recoil';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyles from './styles/GlobalStyles';
-import { theme } from './styles/Theme';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import { theme } from "./styles/Theme";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      suspense: true,
+      // suspense: true,
     },
   },
 });
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <RecoilRoot>
     <QueryClientProvider client={queryClient}>
       <GlobalStyles />
       <ThemeProvider theme={theme}>
         <App />
-        {/* <ReactQueryDevtools initialIsOpen={true} /> */}
+        <ReactQueryDevtools initialIsOpen={true} />
       </ThemeProvider>
     </QueryClientProvider>
   </RecoilRoot>
