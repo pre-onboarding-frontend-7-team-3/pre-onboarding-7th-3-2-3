b1f2c2a871cf2eb8221964f146a154ce6f37458f
leejiwon <365support@naver.com>
2022-11-15 06:07:07 +0900

    feat: 버튼 추가

---
+++ src/components/InvestmentAccountList/component/FilterButton.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const FilterButton = () => {
+  return (
+    <Container>
+      <ButtonContainer></ButtonContainer>
+    </Container>
+  );
+};
+
+export const Container = styled.header`
+  ${({ theme }) => theme.flexDefault}
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 30px;
+  padding: 30px;
+  background-color: aquamarine;
+`;
+
+export const ButtonContainer = styled.div`
+  ${({ theme }) => theme.flexDefault}
+  gap: 18px;
+`;
+
+export default FilterButton;
