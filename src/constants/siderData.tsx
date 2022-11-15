a8edcddb4185a1a5bdfc5aa44c81a612d06b0eec
YeonghunKO <yhko1988@gmail.com>
2022-11-15 23:48:29 +0900

    remove : 사용자목록 필요없음

--- src/constants/siderData.tsx
+++ src/constants/siderData.tsx
@@ -3,5 +3,4 @@ import AssessmentIcon from '@mui/icons-material/Assessment';
 
 export const SIDER_DATA = [
   { id: 1, name: '계좌 목록', keyword: 'accounts', icon: <AssessmentIcon /> },
-  { id: 2, name: '사용자 목록', keyword: 'users', icon: <AccountCircleIcon /> },
 ];
