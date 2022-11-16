975673307ba7199e9ae92e3741673515ee8c14da
Sangmin <23mean@naver.com>
2022-11-17 05:18:17 +0900

    fix: 공통 Table data type을 pageType props에 따라 분리

--- src/components/InvestmentAccountList/InvestmentAccountTableHead/InvestmentAccountTableHead.tsx
+++ src/components/InvestmentAccountList/InvestmentAccountTableHead/InvestmentAccountTableHead.tsx
@@ -1,18 +1,40 @@
 import { TableCell, TableHead, TableRow } from '@mui/material';
 
-const InvestmentAccountTableHead = () => {
+const InvestmentAccountTableHead = ({ pageType }: { pageType: any }) => {
+  const TableType =
+    pageType === "UserDetail"
+      ? [
+          "브로커명",
+          "계좌번호",
+          "계좌상태",
+          "계좌명",
+          "평가금액",
+          "입금금액",
+          "계좌활성화",
+          "계좌개설일",
+        ]
+      : [
+          "고객명",
+          "브로커명",
+          "계좌번호",
+          "계좌상태",
+          "계좌명",
+          "평가금액",
+          "입금금액",
+          "계좌활성화",
+          "계좌개설일",
+        ];
+
   return (
     <TableHead>
       <TableRow>
-        <TableCell align="center">고객명</TableCell>
-        <TableCell align="center">브로커명</TableCell>
-        <TableCell align="center">계좌번호</TableCell>
-        <TableCell align="center">계좌상태</TableCell>
-        <TableCell align="center">계좌명</TableCell>
-        <TableCell align="center">평가금액</TableCell>
-        <TableCell align="center">입금금액</TableCell>
-        <TableCell align="center">계좌활성화</TableCell>
-        <TableCell align="center">계좌개설일</TableCell>
+        {TableType.map((type, idx) => {
+          return (
+            <TableCell key={idx} align="center">
+              {type}
+            </TableCell>
+          );
+        })}
       </TableRow>
     </TableHead>
   );
