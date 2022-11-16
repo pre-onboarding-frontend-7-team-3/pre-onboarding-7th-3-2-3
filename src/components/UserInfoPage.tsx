677a83551e62f4eaf544c74a6326f1f6f8aa1fab
YeonghunKO <yhko1988@gmail.com>
2022-11-16 21:49:56 +0900

    remove : 안쓰는거 삭제

--- src/components/UserInfoPage.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Wrapper = styled.section``;
-
-const UserInfoPage = () => {
-  return <Wrapper>userInfo page</Wrapper>;
-};
-
-export default UserInfoPage;
