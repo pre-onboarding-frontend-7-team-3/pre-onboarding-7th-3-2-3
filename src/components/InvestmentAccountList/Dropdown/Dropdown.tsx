0dae2504daac4a7bd60fb12840ed84241ee04538
YeonghunKO <yhko1988@gmail.com>
2022-11-17 00:39:46 +0900

    fix : dropdown에 표시되게 함

--- src/components/InvestmentAccountList/Dropdown/Dropdown.tsx
+++ src/components/InvestmentAccountList/Dropdown/Dropdown.tsx
@@ -1,14 +1,28 @@
 import * as S from './Dropdown.style';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
+
+type Props = {
+  name: string;
+  data: any;
+  accountQueryParams: any;
+  setAccountQueryParams: Function;
+};
+
+const inputLabel = {
+  broker_id: '브로커명',
+  is_active: '계좌활성화',
+  status: '계좌상태',
+};
 
 const Dropdown = ({
   name,
   data,
   accountQueryParams,
   setAccountQueryParams,
-}: any) => {
+}: Props) => {
   const handleChange = e => {
     const { name, value } = e.target;
     console.log('name', name);
@@ -20,8 +34,12 @@ const Dropdown = ({
 
   return (
     <FormControl sx={S.customDropdownStyle.form} size="small">
+      <InputLabel id="demo-simple-select-label">
+        {inputLabel[name as keyof typeof inputLabel]}
+      </InputLabel>
       <Select
         name={name}
+        label={inputLabel[name as keyof typeof inputLabel]}
         value={accountQueryParams[name]}
         onChange={handleChange}
         inputProps={{ 'aria-label': 'Without label' }}
--- src/components/InvestmentAccountList/InvestmentAccountList.tsx
+++ src/components/InvestmentAccountList/InvestmentAccountList.tsx
@@ -1,21 +1,14 @@
-import { useState } from "react";
-import styled from "styled-components";
-import { Table, TableContainer, Paper } from "@mui/material";
-import { useGetAccountQuery } from "@src/components/InvestmentAccountList/Account-query/InvestmentAccount.query";
-import usePrefetchAccountList from "./hooks/usePrefetchAccountList";
-import InvestmentAccountTableHead from "./InvestmentAccountTableHead/InvestmentAccountTableHead";
-import InvestmentAccountItem from "./InvestmentAccountItem/InvestmentAccountItem";
-import SearchInput from "./component/SearchInput";
-import Dropdown from "./Dropdown/Dropdown";
-import PagenationButton from "./component/PagenationButton";
-import { DROPDOWN_DATA } from "@src/constants/dropDownData";
-
-const PARAMETER_KEYS = {
-  keyword: "",
-  broker_id: "",
-  is_active: "",
-  status: "",
-};
+import { useState } from 'react';
+import styled from 'styled-components';
+import { Table, TableContainer, Paper } from '@mui/material';
+import { useGetAccountQuery } from '@src/components/InvestmentAccountList/Account-query/InvestmentAccount.query';
+import usePrefetchAccountList from './hooks/usePrefetchAccountList';
+import InvestmentAccountTableHead from './InvestmentAccountTableHead/InvestmentAccountTableHead';
+import InvestmentAccountItem from './InvestmentAccountItem/InvestmentAccountItem';
+import SearchInput from './component/SearchInput';
+import Dropdown from './Dropdown/Dropdown';
+import PagenationButton from './component/PagenationButton';
+import { DROPDOWN_DATA } from '@src/constants/dropDownData';
 
 const InvestmentAccountList = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -35,8 +28,8 @@ const InvestmentAccountList = () => {
   // usePrefetchAccountList({ currentPage, maxPage });
 
   const handleCurrentPage = (num: number) => {
-    setCurrentPage((prev) => prev + num);
-    setAccountQueryParams((prev) => {
+    setCurrentPage(prev => prev + num);
+    setAccountQueryParams(prev => {
       return {
         ...prev,
         pageLimit: currentPage + num,
@@ -59,7 +52,7 @@ const InvestmentAccountList = () => {
         {DROPDOWN_DATA.map(({ id, name, data }) => (
           <Dropdown
             key={id}
-            accountQueryParams={PARAMETER_KEYS}
+            accountQueryParams={accountQueryParams}
             setAccountQueryParams={setAccountQueryParams}
             name={name}
             data={data}
