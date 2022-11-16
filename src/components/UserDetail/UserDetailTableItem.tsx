a34c9fe7b4a4eedff20bb8dea101eb4bb26922fa
jungmin <qkrwjdals302@naver.com>
2022-11-16 18:01:38 +0900

    Feat: 유저상세페이지 편집 기능

--- src/components/UserDetail/UserDetailTableItem.tsx
+++ src/components/UserDetail/UserDetailTableItem.tsx
@@ -1,4 +1,6 @@
-import { TableBody, TableCell, TableRow } from "@mui/material";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { TableBody, TableCell, TableRow, InputBase } from "@mui/material";
 import { GENDER } from "@src/constants/tableData";
 import { convertDate } from "@src/utils/convertDate";
 import { formatBoolean } from "@src/utils/formatBoolean";
@@ -7,13 +9,14 @@ import { UserDetailProps, UserSettingProps } from "./types";
 interface Props {
   detail: UserDetailProps;
   setting: UserSettingProps;
+  isModify: boolean;
 }
 
-const UserDetailTableItem = ({ detail, setting }: Props) => {
+const UserDetailTableItem = ({ detail, setting, isModify }: Props) => {
   const usedDatas = [
     detail?.name,
     GENDER[detail?.gender_origin],
-    detail?.birth_date.substring(0, 10), //.split("").slice(0, 10),
+    detail?.birth_date.substring(0, 10),
     detail?.address + detail?.detail_address,
     detail?.email,
     detail?.phone_number,
@@ -22,14 +25,23 @@ const UserDetailTableItem = ({ detail, setting }: Props) => {
     detail?.created_at.substring(0, 10),
     convertDate(detail?.last_login),
   ];
-
-  console.log(usedDatas);
+  const modifiableIdx = 0;
+  const inputProps = (idx: number) => {
+    return isModify && modifiableIdx === idx ? {} : { readOnly: true };
+  };
 
   return (
     <TableBody>
       <TableRow>
-        {detail &&
-          usedDatas.map((data, idx) => <TableCell key={idx}>{data}</TableCell>)}
+        {usedDatas.map((data, idx) => (
+          <TableCell key={idx}>
+            <InputBase
+              css={inputStyle(isModify && modifiableIdx === idx)}
+              {...inputProps(idx)}
+              defaultValue={data}
+            />
+          </TableCell>
+        ))}
       </TableRow>
     </TableBody>
   );
@@ -37,9 +49,10 @@ const UserDetailTableItem = ({ detail, setting }: Props) => {
 
 export default UserDetailTableItem;
 
-/*
-        {detail &&
-          Object.values(detail).map((item, idx) => (
-            <TableCell key={idx}>{item}</TableCell>
-          ))}
-*/
+const inputStyle = (isModify: any) => css`
+  height: 10px;
+  font-weight: 700;
+  font-size: 12px;
+  color: "#3A474E";
+  border: ${isModify ? "solid 2px red" : ""};
+`;
