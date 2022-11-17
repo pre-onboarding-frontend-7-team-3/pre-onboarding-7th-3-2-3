3239c1b290f2483158736265fb2e6b880237060d
leejiwon <365support@naver.com>
2022-11-17 20:44:41 +0900

    Fix: type 1차 설정

--- src/components/NewUserModal/UserInput/UserInput.tsx
+++ src/components/NewUserModal/UserInput/UserInput.tsx
@@ -1,18 +1,31 @@
-import { UseFormRegister, FieldValues } from 'react-hook-form';
-import * as S from './UserInput.style';
+import { UseFormRegister, FieldValues, FieldErrorsImpl } from "react-hook-form";
+import * as S from "./UserInput.style";
 
-type Props = {
+interface Props {
+  inputProps: InputPropsType;
+  errors: Partial<
+    FieldErrorsImpl<{
+      [x: string]: any;
+    }>
+  >;
+  register: UseFormRegister<FieldValues>;
+}
+
+type InputPropsType = {
   type: string;
   text: string;
   name: string;
-  errors: object;
   validation: object;
   autoFocus?: boolean;
   autocomplete: string;
-  register: UseFormRegister<FieldValues>;
+  placeholder: string;
 };
 
 const UserInput = ({ inputProps, errors, register }: Props) => {
+  console.log("inputProps", inputProps);
+  console.log("errors", errors);
+  console.log("register", register);
+
   const { type, name, placeholder, validation, autoFocus, autocomplete } =
     inputProps;
   return (
