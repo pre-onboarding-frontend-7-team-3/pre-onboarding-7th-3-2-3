29c147739b007bb49c3d8fd12c2f1fdf1a9bffdb
moonkorea00 <moonkorea00@gmail.com>
2022-11-17 12:58:44 +0900

    feat : 계좌 테이블 상수데이터 처리

--- src/components/InvestmentAccountList/InvestmentAccountTableHead/InvestmentAccountTableHead.tsx
+++ src/components/InvestmentAccountList/InvestmentAccountTableHead/InvestmentAccountTableHead.tsx
@@ -1,18 +1,15 @@
 import { TableCell, TableHead, TableRow } from '@mui/material';
+import { ACCOUNT_TABLE_HEAD } from '../../../constants/UserTableData';
 
 const InvestmentAccountTableHead = () => {
   return (
     <TableHead>
       <TableRow>
-        <TableCell align="center">고객명</TableCell>
-        <TableCell align="center">브로커명</TableCell>
-        <TableCell align="center">계좌번호</TableCell>
-        <TableCell align="center">계좌상태</TableCell>
-        <TableCell align="center">계좌명</TableCell>
-        <TableCell align="center">평가금액</TableCell>
-        <TableCell align="center">입금금액</TableCell>
-        <TableCell align="center">계좌활성화</TableCell>
-        <TableCell align="center">계좌개설일</TableCell>
+        {ACCOUNT_TABLE_HEAD.map(({ id, text }) => (
+          <TableCell align="center" key={id}>
+            {text}
+          </TableCell>
+        ))}
       </TableRow>
     </TableHead>
   );
