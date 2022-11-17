87261b807675078c32d455e6ab4e50100b29fca0
YeonghunKO <yhko1988@gmail.com>
2022-11-17 21:52:59 +0900

    feat : 새로고침시에도 드랍다운 상태 유지되게 함
    필터초기화 버튼 만듬

--- src/components/InvestmentAccountList/InvestmentAccountList.tsx
+++ src/components/InvestmentAccountList/InvestmentAccountList.tsx
@@ -1,25 +1,17 @@
 import { accountQueryParamsAtom } from './atoms';
 import { useAtom } from 'jotai';
-import styled from "styled-components";
-import { Table, TableContainer, Paper } from "@mui/material";
-import { useGetAccountQuery } from "@src/components/InvestmentAccountList/Account-query/InvestmentAccount.query";
-import InvestmentAccountTableHead from "./InvestmentAccountTableHead/InvestmentAccountTableHead";
-import InvestmentAccountItem from "./InvestmentAccountItem/InvestmentAccountItem";
+import styled from 'styled-components';
+import { Table, TableContainer, Paper, Button } from '@mui/material';
+import { useGetAccountQuery } from '@src/components/InvestmentAccountList/Account-query/InvestmentAccount.query';
+import InvestmentAccountTableHead from './InvestmentAccountTableHead/InvestmentAccountTableHead';
+import InvestmentAccountItem from './InvestmentAccountItem/InvestmentAccountItem';
 
-import Dropdown from "../common/Dropdown/Dropdown";
-import SearchInput from "../common/SearchInput/SearchInput";
-import PagenationButton from "./PagenationButton/PagenationButton";
-import { DROPDOWN_DATA } from "@src/constants/dropDownData";
-
-const PARAMETER_KEYS = {
-  keyword: "",
-  broker_id: "",
-  is_active: "",
-  status: "",
-};
+import Dropdown from '../common/Dropdown/Dropdown';
+import SearchInput from '../common/SearchInput/SearchInput';
+import PagenationButton from './PagenationButton/PagenationButton';
+import { DROPDOWN_DATA } from '@src/constants/dropDownData';
 
 const InvestmentAccountList = () => {
-
   const [accountQueryParams, setAccountQueryParams] = useAtom(
     accountQueryParamsAtom
   );
@@ -28,16 +20,22 @@ const InvestmentAccountList = () => {
     data: defaultAccountListData,
     isLoading,
     isError,
-  } = useGetAccountQuery(accountQueryParams); 
+  } = useGetAccountQuery(accountQueryParams);
 
   const maxPage = defaultAccountListData?.data?.length;
 
   const handlePageNum = (num: number) => {
-    setAccountQueryParams((prev) => {
+    setAccountQueryParams(prev => {
       return {
         ...prev,
         pageNum: num,
       };
+    });
+  };
+
+  const initDropDownFilters = () => {
+    setAccountQueryParams({
+      pageNum: 1,
     });
   };
 
@@ -52,16 +50,25 @@ const InvestmentAccountList = () => {
   return (
     <>
       <Container>
-        <SearchInput onUpdateParams={setAccountQueryParams} text='계좌명 검색'/>
+        <SearchInput
+          onUpdateParams={setAccountQueryParams}
+          text="계좌명 검색"
+        />
         {DROPDOWN_DATA.map(({ id, name, data }) => (
           <Dropdown
             key={id}
-            accountQueryParams={accountQueryParams}
             setAccountQueryParams={setAccountQueryParams}
             name={name}
             data={data}
           />
         ))}
+        <Button
+          sx={{ margin: '8px' }}
+          variant="contained"
+          onClick={initDropDownFilters}
+        >
+          필터링 초기화
+        </Button>
       </Container>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
